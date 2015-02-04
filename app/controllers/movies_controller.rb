671fea0c2205cec0e66f46ed96722bf891287d2c
Raghu Betina <rbetina@github.com>
2015-02-04 16:18:11 -0600

    Fixes bug in controller

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -6,11 +6,11 @@ class MoviesController < ApplicationController
   def update_row
     @movie = Movie.find(params["id"])
 
-    @movie.title = params["the_title"]
-    @movie.year = params["the_year"]
-    @movie.duration = params["the_duration"]
-    @movie.description = params["the_description"]
-    @movie.image_url = params["the_image_url"]
+    @movie.title = params["title"]
+    @movie.year = params["year"]
+    @movie.duration = params["duration"]
+    @movie.description = params["description"]
+    @movie.image_url = params["image_url"]
 
     @movie.save
 
@@ -20,11 +20,11 @@ class MoviesController < ApplicationController
   def create_row
     @movie = Movie.new
 
-    @movie.title = params["the_title"]
-    @movie.year = params["the_year"]
-    @movie.duration = params["the_duration"]
-    @movie.description = params["the_description"]
-    @movie.image_url = params["the_image_url"]
+    @movie.title = params["title"]
+    @movie.year = params["year"]
+    @movie.duration = params["duration"]
+    @movie.description = params["description"]
+    @movie.image_url = params["image_url"]
 
     @movie.save
 
