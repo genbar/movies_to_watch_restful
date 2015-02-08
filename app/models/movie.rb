380ce3859524116d1a5d103ad74f0cf07c5b25de
Arjun Venkataswamy <arjun@example.com>
2015-02-08 17:06:04 -0600

    adds director method in movie class

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,11 @@
 class Movie < ActiveRecord::Base
   validates :title, :uniqueness => { :scope => :year }
+
+  # return the associated director object
+  def director
+    Director.find(self.director_id)
+  end
 end
+
+
+
